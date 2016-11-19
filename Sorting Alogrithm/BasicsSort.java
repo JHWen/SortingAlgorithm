f2133fea2cbd61e71d64e63224460df6c520d07a
ncepujiajia <1940098534@qq.com>
2016-11-19 22:36:46 +0800

    实现冒泡排序

--- Sorting Alogrithm/BasicsSort.java
+++ Sorting Alogrithm/BasicsSort.java
@@ -1,11 +1,29 @@
 public class BasicsSort {
 	/**
 	 * 冒泡排序
-	 * 
+	 * 从小到大
 	 * @param nums
 	 */
 	public static void BubbleSort(int[] nums) {
-		
+		int n = nums.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (nums[j] > nums[j + 1]) {
+					int temp = nums[j];
+					nums[j] = nums[j + 1];
+					nums[j + 1] = temp;
+				}
+			}
+		}
+	}
+	/**
+	 * 改进的冒泡排序
+	 * 设置一个flag标志，判断排序结束
+	 * 避免代码多执行了
+	 * @param nums
+	 */
+	public static void BubbleSortImprove(int[] nums) {
+
 	}
 
 	/**
@@ -29,7 +47,8 @@ public class BasicsSort {
 
 	public static void main(String[] args) {
 		int nums[]={8,5,2,6,9,3,1,4,0,7};
-		SelectionSort(nums);
+//		SelectionSort(nums);
+		BubbleSort(nums);
 		for (int i = 0; i < nums.length; i++) {
 			System.out.print(nums[i]+" ");
 		}
